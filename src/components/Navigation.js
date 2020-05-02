70425bf89dd0fd29fe7b88e4476b3c6692049677
Igor Belem <igortbelem@gmail.com>
2020-05-01 23:32:21 -0400

    fixed highlight on hover for skills

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,29 +2,40 @@ import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 
 const Navigation = (props) => {
-  const sendData = (key) =>{
-    props.onNavSelection(key)
-  }
+  const sendData = (key) => {
+    props.onNavSelection(key);
+  };
   return (
     <Navbar
-      className="nav "
+      className="nav"
       collapseOnSelect
-      expand="lg"
-      fixed="top"
-      bg="dark"
       variant="dark"
+      expand="sm"
+      fixed="top"
       style={{
         opacity: "0.9",
       }}
     >
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto mr-5" onSelect={(selectedKey)=>sendData(selectedKey)}>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse
+        style={{ textAlign: "left", padding: "8px" }}
+        id="basic-navbar-nav"
+      >
+        <Nav
+          className="mr-auto mr-5"
+          style={{ textAlign: "center" }}
+          onSelect={(selectedKey) => sendData(selectedKey)}
+        >
           <Nav.Link eventKey="home">Home</Nav.Link>
           <Nav.Link eventKey="about">About</Nav.Link>
           <Nav.Link eventKey="portfolio">Portfolio</Nav.Link>
           <Nav.Link eventKey="contact">Contact</Nav.Link>
-          <Nav.Link target="_blank" href="https://docs.google.com/document/d/1Yeg_P6NTCpHSk3jo9o0cuLe7PzTUgDUV44DpseqxFTA/edit?usp=sharing">CV</Nav.Link>
+          <Nav.Link
+            target="_blank"
+            href="https://docs.google.com/document/d/1Yeg_P6NTCpHSk3jo9o0cuLe7PzTUgDUV44DpseqxFTA/edit?usp=sharing"
+          >
+            Resume
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Figure } from "react-bootstrap";
+import { Row, Col, Figure, Container } from "react-bootstrap";
 import jsicon from "./media/js_badge.svg";
 import nodeicon from "./media/nodejs_small.svg";
 import cssicon from "./media/css3.svg";
@@ -13,10 +13,11 @@ const Skills = () => {
   return (
     <div className="skills pl-5 pr-5 pb-5 justify-content-center">
       <h1 className="display-4">Skills</h1>
+
       <Row>
-        <Col>
-          <Row className="justify-content-center iconRow">
-            <Col>
+        <Container>
+          <Col>
+            <Row className="justify-content-center">
               <Figure className="skillTab">
                 <Figure.Image src={jsicon} width={68} />
                 <Figure.Caption>JavaScript</Figure.Caption>
@@ -29,10 +30,11 @@ const Skills = () => {
                 <Figure.Image src={reacticon} width={68} />
                 <Figure.Caption>React</Figure.Caption>
               </Figure>
-              <br></br>
+            </Row>
+            <Row className="justify-content-center">
               <Figure className="skillTab">
                 <Figure.Image src={cssicon} width={68} />
-                <Figure.Caption>React</Figure.Caption>
+                <Figure.Caption>CSS3</Figure.Caption>
               </Figure>
               <Figure className="skillTab">
                 <Figure.Image src={bootstricon} width={68} />
@@ -42,9 +44,10 @@ const Skills = () => {
                 <Figure.Image src={html5} width={68} />
                 <Figure.Caption>HTML5</Figure.Caption>
               </Figure>
-              <br></br>
+            </Row>
+            <Row className="justify-content-center">
               <Figure className="skillTab">
-                <Figure.Image src={expressjs} width={68} />
+                <Figure.Image src={expressjs} />
                 <Figure.Caption>Express</Figure.Caption>
               </Figure>
               <Figure className="skillTab">
@@ -55,54 +58,12 @@ const Skills = () => {
                 <Figure.Image src={php} width={68} />
                 <Figure.Caption>PHP</Figure.Caption>
               </Figure>
-            </Col>
-          </Row>
-        </Col>
+            </Row>
+          </Col>
+        </Container>
       </Row>
     </div>
   );
 };
 
 export default Skills;
-/*
-              <div className="skillTab">
-                <img alt="" height="68" src={jsicon} />
-                JavaScript
-              </div>
-              <div className="skillTab">
-                <img alt="" height="68" src={nodeicon} />
-                NodeJS
-              </div>
-              <div className="skillTab">
-                <img alt="" height="68" src={reacticon} />
-                React
-              </div>
-              <br></br>
-              <br></br>
-              <div className="skillTab">
-                <img alt="" height="68" src={cssicon} />
-                CSS3
-              </div>
-              <div className="skillTab">
-                <img alt="" height="68" src={bootstricon} />
-                Bootstrap
-              </div>
-              <div className="skillTab">
-                <img alt="" src={expressjs} />
-                Express
-              </div>
-              <br></br>
-              <br></br>
-              <div className="skillTab">
-                <img alt="" height="68" src={mongodbicon} />
-                MongoDB
-              </div>
-              <div className="skillTab">
-                <img alt="" height="68" src={html5} />
-                HTML5
-              </div>
-              <div className="skillTab">
-                <img alt="" height="68" src={php} />
-                PHP
-              </div>
-              */
