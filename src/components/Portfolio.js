34e2a015e2b333ea1f34ab8367c0019e9a24b43d
Igor Belem <igortbelem@gmail.com>
2021-04-09 19:38:06 -0400

    Restyled portfolio section removed bootstrap

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,58 +1,41 @@
 import React from "react";
 import Project from "./Project";
-import { Row, Col } from "react-bootstrap";
+import "./Portfolio.scss";
 const Portfolio = () => {
   return (
-    <div className="port">
+    <section className="portfolio_container">
       <h1>My Projects</h1>
-      <Row
-        className="justify-content-center"
-        style={{ margin: "auto", paddingBottom: "5%" }}
-      >
-        <Col xs={11} sm={6} lg={4} xl={3}>
-          <Project
-            name={"IMS"}
-            description={
-              "An inventory management system made using the MERN Stack"
-            }
-            repo={"https://github.com/itbel/capstone-abc-store"}
-            pic={"/assets/inventory_list.png"}
-          ></Project>
-        </Col>
-        <Col xs={11} sm={6} lg={4} xl={3}>
-          <Project
-            demo={"http://react-chatapp.crabdance.com/"}
-            name={"ReactChatApplication"}
-            description={
-              "A chat application made using the MERN stack and Socket.IO"
-            }
-            repo={"https://github.com/itbel/ReactChatApplication"}
-            pic={"/assets/chatApp.png"}
-          ></Project>
-        </Col>
-        <Col xs={11} sm={6} lg={4} xl={3}>
-          <Project
-            name={"Bolao APP"}
-            description={
-              "A Soccer tournament result guessing application built with React Native. This shares a backend with footGuess."
-            }
-            repo={"https://github.com/itbel/bolao"}
-            pic={"/assets/bolaoapp.jpg"}
-          ></Project>
-        </Col>
-        <Col xs={11} sm={6} lg={4} xl={3}>
-          <Project
-            demo={"http://bolao-app.crabdance.com"}
-            name={"footGuess"}
-            description={
-              "Soccer Guessing Tournaments Application using MERN Stack"
-            }
-            repo={"https://github.com/itbel/footGuess"}
-            pic={"/assets/footGuess.png"}
-          ></Project>
-        </Col>
-      </Row>
-    </div>
+      <article className="portfolio_grid">
+        <Project
+          demo={"http://react-chatapp.crabdance.com/"}
+          name={"ReactChatApplication"}
+          description={
+            "A chat application made using the MERN stack and Socket.IO"
+          }
+          repo={"https://github.com/itbel/ReactChatApplication"}
+          pic={"/assets/chatApp.png"}
+        ></Project>
+
+        <Project
+          name={"Bolao APP"}
+          description={
+            "A Soccer tournament result guessing application built with React Native. This shares a backend with footGuess."
+          }
+          repo={"https://github.com/itbel/bolao"}
+          pic={"/assets/bolaoapp.jpg"}
+        ></Project>
+
+        <Project
+          demo={"http://bolao-app.crabdance.com"}
+          name={"footGuess"}
+          description={
+            "Soccer Guessing Tournaments Application using MERN Stack"
+          }
+          repo={"https://github.com/itbel/footGuess"}
+          pic={"/assets/footGuess.png"}
+        ></Project>
+      </article>
+    </section>
   );
 };
 
