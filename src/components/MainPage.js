20f617ad77323848ea7bc19ce08b40685723bd9d
Igor Belem <igortbelem@gmail.com>
2021-04-10 22:34:53 -0400

    Removed navigation bar

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Home from "../components/Home";
-import Navigation from "../components/Navigation";
 import About from "../components/About";
 import Portfolio from "../components/Portfolio";
 import Contact from "../components/Contact";
 import Skills from "../components/Skills";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./Theme.scss"
 function MainPage() {
   return (
-    <div className="allShared">
-        <Navigation className="nav"></Navigation>
+    <main>
         <Home></Home>
         <About></About>
         <Skills></Skills>
         <Portfolio></Portfolio>
         <Contact></Contact>
-    </div>
+    </main>
   );
 }
 
