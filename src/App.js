980f32ea8ab26ac20dc20a08d65eccfb502f9fba
Igor Belem <igortbelem@gmail.com>
2021-04-10 22:40:33 -0400

    Removed old css file Removed redundant div tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,14 +9,12 @@ import MainPage from "./components/MainPage"
 import CovidPage from "./components/CovidPage"
 function App() {
   return (
-    <div>
       <Router>
         <Switch>
           <Route exact path="/" component={MainPage}/>
           <Route path="/covid" component={CovidPage}/>
         </Switch>
       </Router>
-    </div>
   );
 }
 
