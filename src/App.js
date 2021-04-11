eea6df6835c6096060b36779b01359e9e82d4d32
Igor Belem <igortbelem@gmail.com>
2021-04-11 13:40:11 -0400

    Code splitting for Covid page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import MainPage from "./components/MainPage"
-import CovidPage from "./components/CovidPage"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MainPage from "./components/MainPage";
+const CovidPage = React.lazy(() => import("./components/CovidPage"));
 function App() {
   return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={MainPage}/>
-          <Route path="/covid" component={CovidPage}/>
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <React.Suspense fallback={<div></div>}>
+          <Route path="/covid" component={CovidPage} />
+        </React.Suspense>
+      </Switch>
+    </Router>
   );
 }
 
