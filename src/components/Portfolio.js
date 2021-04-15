df001103f8add66e9e2377a480316d99679c6b30
Igor Belem <igortbelem@gmail.com>
2021-04-15 01:12:03 -0400

    Portfolio section rework - Added new pictures - Removed card layout - Projects are now in list format

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,36 +4,34 @@ import "./Portfolio.scss";
 const Portfolio = () => {
   return (
     <section className="portfolio_container">
-      <h1>Projects</h1>
-      <article className="portfolio_grid">
       <Project
-          demo={"https://igorbelem.com/covid"}
-          name={"Ontario Covid Vaccinations Tracker"}
-          description={
-            "Uses AWS Lambda and AWS S3, to fetch and store data from the Ontario Data Catalogue"
-          }
-          repo={"https://github.com/itbel/covid-vaccines-tracker-ON"}
-          pic={"/assets/covid.png"}
-        ></Project>
-        <Project
-          name={"Bolao"}
-          description={
-            "Soccer tournaments application built with React Native. Shares a backend with footGuess"
-          }
-          repo={"https://github.com/itbel/bolao_legacy"}
-          pic={"/assets/bolaoapp.jpg"}
-        ></Project>
+        name={"Bolao"}
+        description={
+          "Soccer tournaments result guessing application built with React Native. Shares a backend with footGuess."
+        }
+        repo={"https://github.com/itbel/bolao_legacy"}
+        pic={"/assets/bolao_reduced.png"}
+      ></Project>
 
-        <Project
-          demo={"http://bolao-app.crabdance.com"}
-          name={"footGuess"}
-          description={
-            "Soccer tournaments application built using MERN Stack"
-          }
-          repo={"https://github.com/itbel/footGuess"}
-          pic={"/assets/footGuess.png"}
-        ></Project>
-      </article>
+      <Project
+        demo={"https://igorbelem.com/covid"}
+        name={"Ontario Vaccinations Tracker"}
+        description={
+          "A website to track the number of vaccinations completed in Ontario in the last seven days. It uses AWS Lambda and AWS S3, to retrieve and store data from the Ontario Data Catalogue."
+        }
+        repo={"https://github.com/itbel/covid-vaccines-tracker-ON"}
+        pic={"/assets/covid_laptop.png"}
+      ></Project>
+
+      <Project
+        demo={"http://bolao-app.crabdance.com"}
+        name={"footGuess"}
+        description={
+          "Soccer tournaments result guessing application built using MERN Stack. Hosted on AWS EC2, this application shares a back-end with Bolao."
+        }
+        repo={"https://github.com/itbel/footGuess"}
+        pic={"/assets/footGuess_reduced.png"}
+      ></Project>
     </section>
   );
 };
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,30 +3,34 @@ import "./Project.scss";
 const Project = ({ name, description, repo, pic, demo }) => {
   return (
     <article className="project_container">
-      <h3>{name}</h3>
-      <a target="_blank" href={pic} rel="noopener noreferrer">
-        <img src={pic} alt={`${name} Project`} />
-      </a>
-      <p>{description}</p>
-      <div className="button_container">
-        <a
-          target="_blank"
-          className="button"
-          rel="noopener noreferrer"
-          href={repo}
-        >
-          View code
+      <figure className="project_pic">
+        <a target="_blank" href={pic} rel="noopener noreferrer">
+          <img src={pic} alt={`${name} Project`} />
         </a>
-        {demo !== undefined ? (
+      </figure>
+      <div className="project_desc">
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <div className="button_container">
           <a
             target="_blank"
             className="button"
             rel="noopener noreferrer"
-            href={demo}
+            href={repo}
           >
-            View demo
+            View Code
           </a>
-        ) : null}
+          {demo !== undefined ? (
+            <a
+              target="_blank"
+              className="button"
+              rel="noopener noreferrer"
+              href={demo}
+            >
+              View Demo
+            </a>
+          ) : null}
+        </div>
       </div>
     </article>
   );
