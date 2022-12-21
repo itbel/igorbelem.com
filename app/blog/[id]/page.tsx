ea619218dafb30224a7f16be9e98509d21cd37af
Igor Belem <igortbelem@gmail.com>
2022-12-20 23:03:09 -0300

    Re-added blog page

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,57 @@
+import { BlogSitePost } from "../page";
+
+async function getPost(id: string): Promise<BlogSitePost> {
+  const uri =
+    process.env.NODE_ENV === "development"
+      ? process.env.API_DEV_URI
+      : process.env.API_PROD_URI;
+  const response = await fetch(`${uri}/api/posts/${id}`);
+  if (!response.ok) {
+    console.log(response.statusText);
+    throw new Error("Failed to fetch blog: " + id);
+  }
+  const json: any = await response.json();
+  const { post }: { post: BlogSitePost } = json;
+  return post;
+}
+
+export default async function BlogPost({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const post = await getPost(id);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "8vw 10vw",
+      }}
+    >
+      <h1
+        style={{
+          fontFamily: "Verdana",
+        }}
+      >
+        {post?.title}
+      </h1>
+      <span
+        style={{
+          marginBottom: 60,
+          fontFamily: "sans-serif",
+          marginTop: 4,
+          fontWeight: 100,
+          fontSize: 12,
+        }}
+      >
+        by {post?.author} -{" "}
+        {new Date(parseInt(post.publishedDate)).toLocaleDateString()}
+      </span>
+      <div
+        style={{ fontFamily: "Georgia" }}
+        dangerouslySetInnerHTML={{ __html: post?.content }}
+      ></div>
+    </div>
+  );
+}
--- pages/api/posts/[id].ts
+++ pages/api/posts/[id].ts
@@ -5,11 +5,14 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   try {
-    console.log("Fetching blog posts.... ");
+    console.log("Fetching blog post.... ", req.query.id);
     const client = await clientPromise;
     const db = client.db("portfolio");
-    const posts = await db.collection("posts").find({}).toArray();
-    res.status(200).json({ posts });
+    const post = await db.collection("posts").findOne({
+      _id: req.query.id,
+    });
+    console.log("post", post);
+    res.status(200).json({ post });
   } catch (error) {
     console.error({ error });
     res.status(404).json({ message: "Error" });
