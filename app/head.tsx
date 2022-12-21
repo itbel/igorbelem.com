9319ff3a45cf3864c68f93e8d7f9dc7111dded17
Igor Belem <igortbelem@gmail.com>
2022-12-20 21:51:49 -0300

    Added missing meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,36 @@ export default function Head() {
     <>
       <title>Igor Belem</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Full-Stack Developer" />
+      <meta name="title" content="Igor Belem - Full Stack Developer" />
+      <meta
+        name="description"
+        content="Full stack developer specializing in front-end development using Typescript, ReactJS, React Native, NextJS, and Node. View portfolio of past projects with live demos and code samples on website. Strong understanding of both front-end and back-end web development. Available for new project discussions."
+      />
+      <meta
+        name="keywords"
+        content="full stack developer, front-end development, Typescript, ReactJS, React Native, NextJS, Node, CSS, HTML, AWS Lambda, AWS Amplify, Jest, web development, portfolio, live demos, code samples"
+      />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="author" content="Igor Belem" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="android-chrome-192x192" href="/android-chrome-192x192.png" />
+      <link rel="android-chrome-512x512" href="/android-chrome-512x512.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
     </>
   );
 }
